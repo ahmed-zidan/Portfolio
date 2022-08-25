f741a293fe4d168b9cd572eb6a48cdc96c00d6e1
ahmed zidan <zidanzezo17@gmail.com>
2022-08-25 05:56:06 +0200

    Add project files.

---
+++ Core/Models/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Models
+{
+    public class Address : EntityBase
+    {
+        public string Street { set; get; }
+        public string City { set; get; }
+        public int Number { get; set; }
+    }
+
+}
---
+++ Core/Models/Owner.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Models
+{
+    public class Owner : EntityBase
+    {
+        public string FullName { get; set; }
+        public string Profile { get; set; }
+        public string Avatar { set; get; }
+
+        public Address Address { get; set; }
+
+    }
+
+}
